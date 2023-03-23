976c740aafb16715bbd3238ba3d6d7e0b8307ae3
lakij <lazar.jovanovic125@gmail.com>
2023-03-23 14:46:37 +0100

    bug fixes

--- app/src/main/java/lazar/jovanovic/shoppinglist/NewList.java
+++ app/src/main/java/lazar/jovanovic/shoppinglist/NewList.java
@@ -2,20 +2,23 @@ package lazar.jovanovic.shoppinglist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class NewList extends AppCompatActivity implements View.OnClickListener {
 
-    Button bOk;
+    Button bOk, bSave;
     TextView naslov;
     EditText edNaslov;
     RadioButton bYes;
     RadioButton bNo;
+    boolean ok_pressed = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,13 +29,31 @@ public class NewList extends AppCompatActivity implements View.OnClickListener {
         edNaslov = findViewById(R.id.naslov_liste);
         bYes = findViewById(R.id.yes_button);
         bNo = findViewById(R.id.no_button);
+        bSave = findViewById(R.id.save_button);
 
         bOk.setOnClickListener(this);
+        bSave.setOnClickListener(this);
 
     }
-
     @Override
     public void onClick(View view) {
-        naslov.setText(edNaslov.getText().toString());
+        switch (view.getId()){
+            case R.id.naslov_ok:{
+                naslov.setText(edNaslov.getText().toString());
+                edNaslov.getText().clear();
+                ok_pressed = true;
+                break;
+            }
+            case R.id.save_button:{
+                if(ok_pressed == true){
+                    Intent intent = new Intent(this, WelcomeActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(this, "A list must have a headline", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+
     }
 }
